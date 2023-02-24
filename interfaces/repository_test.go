16d86aa8553d220a375bd148d0209bf15ccc7dd5
yamadatarou <taiko1okrock@gmail.com>
2023-02-24 16:54:22 +0900

    testCode編集

--- interfaces/repository_test.go
+++ interfaces/repository_test.go
@@ -15,50 +15,50 @@ import (
 	"github.com/tmkshy1908/Portfolio/usecase"
 )
 
-var i usecase.CommonRepository
-var contents *domain.Contents
-var bot line.Client
-var Db db.SqlHandler
-var err error
-var userId string
-
-func Newinit() {
-	// var err error
-	err = godotenv.Load("../.env")
-	if err != nil {
-		fmt.Println(err)
-	}
-	bot, err = line.NewClient()
-	if err != nil {
-		fmt.Println(err)
-	}
-	Db, err = db.NewHandler()
-	if err != nil {
-		fmt.Println(err)
-	}
-	i = &interfaces.CommonRepository{DB: Db, Bot: bot}
-	userId = "U024602d41dff0396b1630a44ccd5dbee"
-
-	// contents = &domain.Contents{Contents_Day: tt, Location: "池袋", EventTitle: "イベントイベント", Act: "吉田ヨシダ　田中たなか　岡田okada", OtherInfo: "19:00 end"}
+type test_st struct {
+	commonRepository usecase.CommonRepository
+	contents         *domain.Contents
+	userId           string
 }
 
-func Test_Add(t *testing.T) {
-	Newinit()
-	ctx := context.Background()
+func Newinit() (ts *test_st) {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		fmt.Println(err)
+	}
+	bot, err := line.NewClient()
+	if err != nil {
+		fmt.Println(err)
+	}
+	Db, err := db.NewHandler()
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	str := "2020/08/20"
 	layout := "2006/01/02"
 	tt, _ := time.Parse(layout, str)
-	contents = &domain.Contents{Contents_Day: tt, Location: "池袋", EventTitle: "イベントイベント", Act: "吉田ヨシダ　田中たなか　岡田okada", OtherInfo: "19:00 end"}
-	err = i.Add(ctx, contents)
+
+	ts = &test_st{commonRepository: &interfaces.CommonRepository{DB: Db, Bot: bot},
+		contents: &domain.Contents{Contents_Day: tt, Location: "池袋", EventTitle: "イベントイベント", Act: "吉田ヨシダ　田中たなか　岡田okada", OtherInfo: "19:00 end"},
+		userId:   "U024602d41dff0396b1630a44ccd5dbee",
+	}
+	return
+}
+
+func Test_Add(t *testing.T) {
+	ts := Newinit()
+	ctx := context.Background()
+	err := ts.commonRepository.Add(ctx, ts.contents)
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
 func Test_Find(t *testing.T) {
-	Newinit()
+	ts := Newinit()
 	ctx := context.Background()
-	a, err := i.Find(ctx)
+	a, err := ts.commonRepository.Find(ctx)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -66,50 +66,44 @@ func Test_Find(t *testing.T) {
 }
 
 func Test_Update(t *testing.T) {
-	Newinit()
+	ts := Newinit()
 	ctx := context.Background()
-	str := "2020/08/20"
-	layout := "2006/01/02"
-	tt, _ := time.Parse(layout, str)
-	contents = &domain.Contents{Contents_Day: tt, Location: "池袋", EventTitle: "アップデート", Act: "アプデーと", OtherInfo: "19:00 end"}
-	i.Update(ctx, contents)
+	ts.contents.Act = "アップデート"
+	contents := ts.contents
+	ts.commonRepository.Update(ctx, contents)
 }
 
 func Test_Delete(t *testing.T) {
-	Newinit()
+	ts := Newinit()
 	ctx := context.Background()
-	str := "2020/08/20"
-	layout := "2006/01/02"
-	tt, _ := time.Parse(layout, str)
-	contents = &domain.Contents{Contents_Day: tt, Location: "池袋", EventTitle: "イベントイベント", Act: "吉田ヨシダ　田中たなか　岡田okada", OtherInfo: "19:00 end"}
-	i.Delete(ctx, contents)
+	ts.commonRepository.Delete(ctx, ts.contents)
 }
 
 func Test_CallReply(t *testing.T) {
-	Newinit()
+	ts := Newinit()
 	msg := "テストテスト"
 
-	i.CallReply(msg, userId)
+	ts.commonRepository.CallReply(msg, ts.userId)
 
 }
 
 func Test_WaitMsg(t *testing.T) {
-	Newinit()
+	ts := Newinit()
 	ctx := context.Background()
-	contents, err := i.WaitMsg(ctx)
+	contents, err := ts.commonRepository.WaitMsg(ctx)
 	fmt.Println(contents, err)
 }
 
 func TestStartUser(t *testing.T) {
-	Newinit()
+	ts := Newinit()
 	ctx := context.Background()
-	i.StartUser(ctx, userId)
+	ts.commonRepository.StartUser(ctx, ts.userId)
 }
 
 func Test_UserCheck(t *testing.T) {
-	Newinit()
+	ts := Newinit()
 	ctx := context.Background()
-	if i.UserCheck(ctx, userId) {
+	if ts.commonRepository.UserCheck(ctx, ts.userId) {
 		fmt.Println("登録ずみ")
 	} else {
 		fmt.Println("未登録")
@@ -117,7 +111,7 @@ func Test_UserCheck(t *testing.T) {
 }
 
 func Test_EndUser(t *testing.T) {
-	Newinit()
+	ts := Newinit()
 	ctx := context.Background()
-	i.EndUser(ctx, userId)
+	ts.commonRepository.EndUser(ctx, ts.userId)
 }
